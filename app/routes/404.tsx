866af0fd9ccd2b57c3215ab547e6c4849a025924
kusira <takaaki200302@gmail.com>
2024-03-04 21:20:58 +0900

    add:404ページとローディングコンポーネント

---
+++ app/routes/404.tsx
@@ -0,0 +1,33 @@
+import { Link } from "@remix-run/react"
+import { Button } from "~/components/ui/button"
+
+const NotFound = () => {
+  return (
+    <div className="bg-white py-6 sm:py-8 lg:py-12 translate-y-[100px]">
+      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+        <div className="flex flex-col items-center">
+
+        <Link to="/" className="mb-1">
+          <div className="flex items-center gap-4 h-[60px]">
+            <img src="images/logo/komai_logo.png" alt="logo" className="w-auto h-[50px] object-fill"/>
+            <h1 className="flex h-full items-center text-4xl font-bold">KOM-4i</h1>
+          </div>
+        </Link>
+
+
+          <p className="mb-4 text-sm font-semibold uppercase text-[#f6511d] md:text-base">That’s a 404</p>
+          <h1 className="mb-2 text-center text-2xl font-bold text-gray-800 md:text-3xl">Page not found</h1>
+
+          <p className="mb-12 max-w-screen-md text-center text-gray-500 md:text-lg">お探しのページは存在しません</p>
+          <Link to="/">
+            <Button className="inline-block rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base">
+              ホームに戻る
+            </Button >
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -33,7 +33,6 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
 
-
   return (
     <div>
       <Header />
