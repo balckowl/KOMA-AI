98bd2a59610278bcd5486b5baf1ec3dbf4b4a056
kusira <takaaki200302@gmail.com>
2024-03-08 22:16:41 +0900

    fix:テキストが途中で切れるバグを修正しました

--- app/routes/components/create/step2.tsx
+++ app/routes/components/create/step2.tsx
@@ -101,8 +101,8 @@ const Step2 = ({ setStep, yonkoma, setYonkoma, postId }: { setStep: any, yonkoma
             </h3>
             {yonkoma.map((koma: any, index: number) => (
               <div className=" border-black border-[2px] mb-5 bg-white" key={index}>
-                <div className="flex justify-start items-center gap-3 p-2">
-                  <div className="text-[14px] w-full h-max md:h-[54px]  bg-transparent resize-none overflow-hidden">
+                <div className="flex justify-start items-center gap-3 p-2 h-[70px]">
+                  <div className="text-[14px] w-full bg-transparent resize-none overflow-hidden h-max">
                     {isFinishList[index] ? (
                       // アニメーションが終わった
                       <textarea
